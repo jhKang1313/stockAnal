53980f250cc328a899e29beab44bc134fd0d7b65
jhkang1313 <jhkang1313@gmail.com>
2024-01-02 22:55:29 +0900

    DataFrame 가져오기 방법
    Date는 index 는 나머지는 column 으로 접근

--- Stock.py
+++ Stock.py
@@ -6,8 +6,8 @@ bot = sb.StockBot(fromDate = "20231201", toDate = "20231231")
 bot.getTop1Kospi()
 
 stock1 = bot.get("005930")
-bot.saveToExcel(stock1)
 
-#Test
-#아안녕하세요
-#ㄴㅇㅁㄴㅇㅁㄴ
+print(stock1.loc["2023-12-04"]["Open"])
+# Date -> Index 필드로 지정할 수 있음
+print(stock1.index)
+
