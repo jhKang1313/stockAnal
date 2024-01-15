076ee24321c804cbe7440ef7242d46494b99d045
jhkang1313 <jhkang1313@gmail.com>
2024-01-15 23:53:10 +0900

    가져오기 수정 및 분석 클래스 추가

--- Stock.py
+++ Stock.py
@@ -1,9 +1,10 @@
 import StockBot as sb
 from datetime import datetime as dt
-#print(dir(datetime))
-bot = sb.StockBot(fromDate = "20230101", toDate = "20240110")
-#bot.getTop10Kospi()
-#bot.getTop1Kospi()
+import StockAnal as sa
+
+today = dt.strftime(dt.today(), '%Y%m%d')
+fromDate = "20220101"
+bot = sb.StockBot(fromDate = fromDate, toDate = today)
 
 stock1 = bot.get("012510")
 #bot.print("012510")
@@ -11,13 +12,17 @@ stock1 = bot.get("012510")
 # Date -> Index 필드로 지정할 수 있음
 
 
+for id in bot.df.keys():
+  tt = sa.StockAnal(bot, id, today)
+  tt.print()
+  
+  
+  
+
 #today = dt.strftime(dt.now(), '%Y-%m-%d')
 #print(stock1.loc[today]["Close"])
 
-range = 500 #500
-
-for key in stock1.index:
-    stock1.loc[key]["Close"]
+#for key in stock1.index:
 
 
 
