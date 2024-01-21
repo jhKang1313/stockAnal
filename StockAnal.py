d7b6d3b3c47e787d3210e0f26ba2388fb0797f83
jhkang1313 <jhkang1313@gmail.com>
2024-01-21 23:57:48 +0900

    Get MinRows
    - Min Date
    - Diff Close Amount between Min Rows Close
    - Diff days

--- StockAnal.py
+++ StockAnal.py
@@ -1,4 +1,7 @@
 import locale as lc
+from datetime import datetime as dt
+
+FDATE = "%Y%m%d"
 class StockAnal:    
     def __init__(self, bot, today):
         self.baseCol = "Close"
@@ -11,19 +14,21 @@ class StockAnal:
         self.stock = self.bot.get(id)
         self.minRow = self.stock.loc[self.stock[self.baseCol].idxmin()]
         self.min = self.minRow[self.baseCol]
-        self.minDate = self.minRow.name.strftime("%Y-%m-%d")
+        self.minDate = self.minRow.name
+        self.minDateStr = self.minDate.strftime(FDATE)
         self.todayAmt = self.stock.loc[self.today][self.baseCol]
         self.todayDiffAmt = self.todayAmt - self.min
+        self.todayDiffDays = (dt.strptime(self.today, FDATE) - self.minDate).days
     def doAnal(self):
-        
         pass
     def print(self, all=False):
         if all == True:
             self.bot.print(self.id)
         print(f"-min : {self.doLocale(self.min)}")
-        print(f"-minDate : {self.minDate}")
+        print(f"-minDate : {self.minDateStr}")
         print(f"-todayAmt : {self.doLocale(self.todayAmt)}")
         print(f"-today Diff Amt : {self.doLocale(self.todayDiffAmt)}")
+        print(f"-today Diff Days : {self.todayDiffDays}")
     def doLocale(self, num):
         return lc.format_string('%d', num, grouping=True)
         
